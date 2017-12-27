e8062d7ad2f37afa70e05a067ffab08d130fd8c5
Tamil Vendhan <tamil@mymandir.com>
2017-12-27 22:30:35 +0800

    Removed console.log

--- public/src/musicoin-forum.js
+++ public/src/musicoin-forum.js
@@ -1,5 +1,4 @@
 require(['components'], function(components) {
-	console.log('Overriding logout');
   components.get('user/logout').on('click', function() {
   	windown.location.href = config.appURL + '/logout?returnTo=' + config.forumURL;
   });
