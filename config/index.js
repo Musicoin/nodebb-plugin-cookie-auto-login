00d4d940dc480862de6c07c288fabe4fcb865f3a
Varunram Ganesh <vrg2009@ymail.com>
2018-06-13 19:18:32 +0530

    Delete staging, use main forum

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 
 const appURL = process.env.NODE_ENV === 'development' ? 'https://staging.musicoin.org' : 'https://staging.musicoin.org';
-const forumURL = process.env.NODE_ENV === 'development' ? 'https://forum-staging.musicoin.org' : 'https://forum.musicoin.org';
+const forumURL = process.env.NODE_ENV === 'development' ? 'https://forum.musicoin.org' : 'https://forum.musicoin.org';
 
 module.exports = {
 	appURL: appURL,
